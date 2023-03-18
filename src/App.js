1f7fa1b00bfc50b60b22513f2c426c414e697f19
Onkar Pathak <112766269+Onkar432@users.noreply.github.com>
2023-03-18 19:41:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'; //To App.css which contains style for this app we have to include it first
+
+function App() {
+  return (
+    <>
+   <h1>Hello</h1>
+   <Demo/>
+   </>
+  );
+}
+function Demo(){
+  return(
+    <h1>Demo</h1>
+  );
+}
+export default App;
+
+
+
+
